156077a38197c48695d63afffa7ca700f60a6a66
Ag-chr <agc@aktivit.dk>
2024-11-27 22:08:56 +0100

    Lavet treeview en klasse

--- hjælpeFunktioner.py
+++ hjælpeFunktioner.py
@@ -13,3 +13,35 @@ def moneySortedList():
 def nameSortedList():
     pass
 
+from tkinter import *
+from tkinter.ttk import Treeview
+import pandas as pd
+class MakeTree:
+    def __init__(self, master, df: pd.DataFrame):
+        self.master = master
+        self.df = df
+        self.l1 = list(self.df)  # list of columns
+        self.order = True
+
+        self.trv = Treeview(self.master, selectmode='browse', show="headings", height=10, columns=self.l1)
+        self.trv.pack()
+
+    # fylder tabel med information
+    def my_disp(self):
+        r_set = self.df.to_numpy().tolist()
+        for col in self.l1:
+            self.trv.column(col, width=200, anchor='w')
+            self.trv.heading(col, text=col, command=lambda col=col: self.my_sort(col)) # col=col gør så den sortere. Ellers virker det ikke
+        for dt in r_set:
+            v = [r for r in dt]
+            self.trv.insert('', 'end', values=v)
+
+    def my_sort(self, col):
+        if self.order:
+            self.order = False
+        else:
+            self.order = True
+        self.df = self.df.sort_values([col], ascending=self.order)
+        self.trv.delete(*self.trv.get_children())
+        self.my_disp()
+
--- listWindow.py
+++ listWindow.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import Treeview
 import pandas as pd
+from hjælpeFunktioner import MakeTree
 
 class listWindowClass:
     def __init__(self, master):
@@ -15,37 +16,17 @@ class listWindowClass:
         fodboldturDict["Resterende"] = [self.master.dkk_pr_medlem - indbetalt for indbetalt in fodboldturDict["Indbetalt"]]
 
         self.df = pd.DataFrame.from_dict(fodboldturDict)
-        self.order=True
         self.l1 = list(self.df)  # list of columns
+        self.order = True
 
         # Overskrift
         Label(self.listWindow, text="Indbetalingsliste").pack(pady=10)
 
-        self.trv = Treeview(self.listWindow, selectmode='browse', show="headings", height=10, columns=self.l1)
-        self.trv.pack()
-        #Laver en tabel
-        self.my_disp()
+        self.trv = MakeTree(self.listWindow, self.df)
+        self.trv.my_disp()
 
         # Tilbage til main menu knap
         Button(self.listWindow, text="Tilbage", command=self.listWindow.destroy).pack()
-
-    def my_disp(self):
-        r_set = self.df.to_numpy().tolist()
-        for col in self.l1:
-            self.trv.column(col, width=200, anchor='w')
-            self.trv.heading(col, text=col, command=lambda col=col: self.my_sort(col))
-        for dt in r_set:
-            v = [r for r in dt]
-            self.trv.insert('', 'end', values=v)
-
-    def my_sort(self, col):
-        if self.order:
-            self.order = False
-        else:
-            self.order = True
-        self.df = self.df.sort_values([col], ascending=self.order)
-        self.trv.delete(*self.trv.get_children())
-        self.my_disp()
 
     def FyldTabel(self):
         # Fylder tabellen  med data
